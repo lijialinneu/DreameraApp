db986b01169c2ed0409b5a7a95dea5ea4678f957
lijialin <1040591521@qq.com>
2016-08-26 11:46:32 +0800

    set the map1/map2 northeast and southeast

--- app/src/main/java/neu/dreamerajni/utils/BMapControlUtil.java
+++ app/src/main/java/neu/dreamerajni/utils/BMapControlUtil.java
@@ -49,15 +49,14 @@ import neu.dreamerajni.utils.MyOrientationListener.OnOrientationListener;
 public class BMapControlUtil {
 
     @Bind(R.id.id_bmapView)
-    public MapView mapView;     //绑定地图控件
+    public MapView mapView;                                  //绑定地图控件
 
-    public Activity activity;   //调用BmMap控件的Activity
-    public BaiduMap baiduMap;   //地图实例
+    public Activity activity;                                //调用BmMap控件的Activity
+    public BaiduMap baiduMap;                                //地图实例
     private MarkerPopupWindowView markerPopupWindowView;     //底部弹窗
     public static RelativeLayout map;                        //地图的容器
     private boolean isFristLocation = true;                  //第一次定位
-    public MyLocationListener mMyLocationListener;           //监听器
-    public LocationMode mCurrentMode = LocationMode.NORMAL;  //定位模式
+    private LocationMode mCurrentMode = LocationMode.NORMAL;  //定位模式
     public LocationClient mLocationClient;                   //客户端
     private int mXDirection;                                 //方向传感器X方向的值
     private double mCurrentLantitude;                        //经度
@@ -66,20 +65,19 @@ public class BMapControlUtil {
     public MyOrientationListener myOrientationListener;      //方向传感器的监听器
 
     /**
-     * 以下与老地图相关
+     * The variable below is related to old map.
      */
     public GroundOverlay oldMapOverlay;
     private LatLng southwest;
     private LatLng northeast;
     private LatLngBounds bounds;
     public BitmapDescriptor bdGround;
-    public OverlayOptions ooGround;
-
+    private OverlayOptions ooGround;
 
 
     /**
-     * 构造函数
-     * @param activity
+     * Constructor
+     * @param activity the activity
      */
     public BMapControlUtil(final Activity activity) {
         this.activity = activity;
@@ -88,28 +86,31 @@ public class BMapControlUtil {
     }
 
     /**
-     * 初始化地图
+     * Initialize baidu map
      */
-    public void initBMap(){
+    private void initBMap(){
         initMyLocation();       // 初始化定位
         initOritationListener();// 初始化方向传感器
 
         markerPopupWindowView = new MarkerPopupWindowView(activity);
         map = (RelativeLayout) activity.findViewById(R.id.id_map);
         baiduMap = mapView.getMap();
+
         baiduMap.setOnMapLoadedCallback(callBackHandler);
         baiduMap.setOnMarkerClickListener(markerClickHandler);
 
         initMarker();//初始化地图标注点
+
     }
 
+
     /**
-     * 初始化定位
+     * Initialize location
      */
     private void initMyLocation(){
         // 定位初始化
         mLocationClient = new LocationClient(activity);
-        mMyLocationListener = new MyLocationListener();
+        MyLocationListener mMyLocationListener = new MyLocationListener();
         mLocationClient.registerLocationListener(mMyLocationListener);
         // 设置定位的相关配置
         LocationClientOption option = new LocationClientOption();
@@ -121,9 +122,9 @@ public class BMapControlUtil {
     }
 
     /**
-     * 实现实位回调监听
+     * Get present location
      */
-    public class MyLocationListener implements BDLocationListener {
+    private class MyLocationListener implements BDLocationListener {
         @Override
         public void onReceiveLocation(BDLocation location){
             // map view 销毁后不在处理新接收的位置
@@ -152,7 +153,7 @@ public class BMapControlUtil {
     }
 
     /**
-     * 初始化方向传感器
+     * Initialize map orientation listener
      */
     private void initOritationListener(){
         myOrientationListener = new MyOrientationListener(activity);
@@ -178,10 +179,8 @@ public class BMapControlUtil {
     }
 
 
-
     /**
-     * 地图回调
-     * @author 10405
+     * Map call back
      */
     OnMapLoadedCallback callBackHandler  = new OnMapLoadedCallback() {//回调函数
             @Override
@@ -193,7 +192,7 @@ public class BMapControlUtil {
                         new LatLngBounds.Builder().include(northeast).include(southwest).build()
                 );
                 baiduMap.setMapStatus(MapStatusUpdateFactory.newMapStatus(
-                        new MapStatus.Builder().zoom(15).build()
+                        new MapStatus.Builder().zoom(18).build()
                 ));
                 if (isFristLocation){                // 第一次定位时，将地图位置移动到当前位置
                     isFristLocation = false;
@@ -205,8 +204,7 @@ public class BMapControlUtil {
     };
 
     /**
-     * 标注点点击
-     * @author 10405
+     * Marker clickListener
      */
     OnMarkerClickListener markerClickHandler = new OnMarkerClickListener() {
         @Override
@@ -231,8 +229,8 @@ public class BMapControlUtil {
 
 
     /**
-     * 初始化地图标注点
-     * @author 10405
+     * Initialize marker clickerListener
+     * Call addMarker function.
      */
     public void initMarker() {
         AsyncGetDataUtil.getJSONData();//准备好加载的数据
@@ -246,14 +244,12 @@ public class BMapControlUtil {
             jsonFromFile = AsyncGetDataUtil.getJSONFromFile();//JSON数据从文件缓存中读到内存中
         }
         addMarkerOnMap(jsonFromFile);
-
     }
 
 
     /**
-     * 在地图上加载标注点
-     * @author 10405
-     * @param jsonString
+     * Add marker on map
+     * @param jsonString json from file
      */
     public void addMarkerOnMap(String jsonString) {
         ArrayList<HashMap<String, Object>> pointList = null;
@@ -263,6 +259,8 @@ public class BMapControlUtil {
             e.printStackTrace();
         }
 
+//        List<MyItem> items = new ArrayList<MyItem>();
+
         for(int i = 0; i < pointList.size(); i++){
 
             //在地图上添加标注点marker
@@ -271,6 +269,8 @@ public class BMapControlUtil {
                     Double.parseDouble(item.get("latitude").toString()),
                     Double.parseDouble(item.get("longitude").toString())
             );// 标注点的经纬度
+
+//            items.add(new MyItem(latLngMarker));
 
             BitmapDescriptor mIconMaker = BitmapDescriptorFactory.fromResource(R.mipmap.ic_marker);
             OverlayOptions overlayOptions = new MarkerOptions()//添加图片标注
@@ -291,12 +291,14 @@ public class BMapControlUtil {
             bundle.putSerializable("cross_pictures", item.get("cross_pictures").toString());
             marker.setExtraInfo(bundle);
         }
+
+//        clusterManager.addItems(items);
     }
 
 
     /**
-     *  显示老地图
-     *  一个没啥大用的功能
+     *  This function is used to show old map.
+     *  The code needed to be changed
      */
     public void addOldMapOverlay(int progress) {
 
@@ -308,22 +310,19 @@ public class BMapControlUtil {
             System.gc();
         }
 
-
         //定义Ground的显示地理范围
-        southwest = new LatLng(41.807449,123.400122);
         northeast = new LatLng(41.755163,123.489881);
-        bounds = new LatLngBounds.Builder()
-                .include(northeast)
-                .include(southwest)
-                .build();
+        southwest = new LatLng(41.807449,123.400122);
 
         //定义Ground显示的图片
         if(progress == 0) {
             bdGround = BitmapDescriptorFactory.fromResource(R.mipmap.map1);
-
-
+            northeast = new LatLng(41.83242,123.488156); //Override northeast above
+            southwest = new LatLng(41.782737,123.431634);//Override southeast above
         } else if(progress == 1) {
             bdGround = BitmapDescriptorFactory.fromResource(R.mipmap.map2);
+            northeast = new LatLng(41.838708,123.490671);
+            southwest = new LatLng(41.782441,123.382227);
         } else if(progress == 2) {
             bdGround = BitmapDescriptorFactory.fromResource(R.mipmap.map3);
         } else if(progress == 3) {
@@ -331,6 +330,11 @@ public class BMapControlUtil {
         } else {
             bdGround = BitmapDescriptorFactory.fromResource(R.mipmap.map5);
         }
+
+        bounds = new LatLngBounds.Builder()
+                .include(northeast)
+                .include(southwest)
+                .build();
 
         //定义Ground覆盖物选项
         ooGround = new GroundOverlayOptions()
